4072433d556b1a635655adb126811a765d76c931
LEEMINJOO <eminj1997@naver.com>
2019-12-25 03:25:49 +0900

    이번 결과 load 후 저장

--- get_youtube_url.py
+++ get_youtube_url.py
@@ -8,28 +8,29 @@ from pytube import Playlist, YouTube
 
 def save(links):
     df = pd.DataFrame({'links':links})
-    df.to_csv('./data/urls.csv', index=False)
+    df.to_csv('./data/links.csv', index=False)
 
-links = []
-count = 0
-main_link = "https://www.youtube.com"
+if __name__ == '__main__':
+    links = list(pd.read_csv('./data/links.csv').links)
+    count = len(links)
+    main_link = "https://www.youtube.com"
 
-playlist_urls = ['플레이리스트 링크들']
+    playlist_urls = ['랑크들']
 
-for playlist_url in playlist_urls:
-    pl = Playlist(playlist_url)
-    for tmp_link in pl.parse_links():
-        link = main_link + tmp_link
-        try: # 비공개영상 접근 불가, key error
-            yt = YouTube(link) 
-            links.append(link)
-            count += 1
-            print(count)
-        except:
-            print('Except')
-        if count%100 == 1:
-            save(links)
-            print('Saved')
-            
-save(links)
-print('Saved')
+    for playlist_url in playlist_urls:
+        pl = Playlist(playlist_url)
+        for tmp_link in pl.parse_links():
+            link = main_link + tmp_link
+            try: # 비공개영상 접근 불가, key error
+                yt = YouTube(link) 
+                links.append(link)
+                count += 1
+                print(count)
+            except:
+                print('Except')
+            if count%100 == 1:
+                save(links)
+                print('Saved')
+
+    save(links)
+    print('Saved')
